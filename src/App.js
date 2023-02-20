2cca8a4f984053fec9c66c93d0149814f8df76a7
vongnx0933 <101400323+vongnx0933@users.noreply.github.com>
2023-02-21 00:37:37 +0700

    push up repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import queryString from 'query-string';
+import Pagination from './components/Panigation';
+import PostList from './components/PostList';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I Love You ' },
+    { id: 2, title: 'We Love You' },
+    { id: 3, title: 'They Love You' },
+  ]);
+
+  const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 15,
+  });
+
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+  })
+
+
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const paramsString = queryString.stringify(filters);
+        const resquestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
+        const response = await fetch(resquestUrl);
+        const responseJSON = await response.json();
+        console.log({ responseJSON });
+        const { data, pagination } = responseJSON;
+        setPostList(data);
+        setPagination(pagination);
+
+      } catch (error) {
+        console.log('Failed to fetch post list', error.message);
+      }
+    }
+    fetchPostList();
+  }, [filters]);
+
+  function handlePageChange(newPage) {
+    console.log('New page:', newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    })
+  }
+
+
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+  function handleTodoFormSubmit(formValues) {
+    console.log("Form submit:", formValues);
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hook - Post List</h1>
+
+      <PostList posts={postList} />
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
     </div>
   );
 }
---
+++ src/components/PostList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+PostList.propTypes = {
+    post:PropTypes.array,
+};
+PostList.defaultProps={
+    post:[],
+}
+
+function PostList(props) {
+    const {posts} = props;
+
+    return (
+       <ul className='post-list'>
+            {posts.map(post =>(
+                <li key={post.id}> {post.title}</li>
+            ))}
+       </ul>
+    );
+}
+
+export default PostList;
